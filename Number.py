41c6bc55e0de10b4200d5601ac9d46a57d5bf579
KiruiTito <128330106+KiruiTito@users.noreply.github.com>
2025-06-10 07:41:30 +0300

    Create Number.py

---
+++ Number.py
@@ -0,0 +1,9 @@
+number = float(input("Enter a number: "))
+
+# Check and display the result
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
